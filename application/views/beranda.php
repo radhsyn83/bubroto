3ab2797a2582f198512e7905093c1bf66a5a0f69
radhsyn83 <radhsyn83@gmail.com>
2019-11-24 21:01:30 +0700

    first commit

---
+++ application/views/beranda.php
@@ -0,0 +1,80 @@
+<div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+	<div class="row mr-0 h-100">
+		<!--Indicator-->
+		<div class="col-md-1">
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="0"></li>
+				<li data-target="#myCarousel" data-slide-to="0"></li>
+			</ol>
+		</div>
+		<!--content-->
+		<div class="col-md-11 p-0">
+			<div class="carousel-inner mr-0 h-100">
+				<div class="carousel-item active">
+					<div class="container h-100 p-0">
+						<div class="row align-items-center h-100">
+							<div class="col-md-5 col-12 order-md-1 order-2">
+								<h4>360 Tour</h4>
+								<p>Jelajahi unikom sekarang hanya dengan <br>
+									sentuhan jari saja.</p>
+								<a class="tour" href="#tour" onclick="menuOpen('.tour', 'tour');">Selengkapnya</a></div>
+							<div class="col-md-7 col-12 order-md-2 order-1">
+								<div class="slider-card container-fluid p-0">
+									<div class="card-item-content d-flex flex-row flex-nowrap">
+										<div class="col-4">
+											<div class="card bg-transparent text-white" style="height: 300px">
+												<div class="img-bg h-100 w-auto"
+													 style="background-image: url(<?= base_url('assets/images/tour/miracle-building-unikom.jpg') ?>);">
+													<div class="img-title card-img-bottom">
+														<span class="card-title">UNIKOM Miracle Building</span>
+														<img class="logo"
+															 src="<?= base_url() ?>assets/images/360.png"/>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="col-4">
+											<div class="card bg-transparent text-white" style="height: 300px">
+												<div class="img-bg h-100 w-auto"
+													 style="background-image: url(<?= base_url('assets/images/tour/smart-building-unikom.jpg') ?>);">
+													<div class="img-title card-img-bottom">
+														<span class="card-title">UNIKOM Smart Building</span>
+														<img class="logo"
+															 src="<?= base_url() ?>assets/images/360.png"/>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="col-4">
+											<div class="card bg-transparent text-white" style="height: 300px">
+												<div class="img-bg h-100 w-auto"
+													 style="background-image: url(<?= base_url('assets/images/tour/pasca-sarjana-unikom-dago.jpg') ?>);">
+													<div class="img-title card-img-bottom">
+														<span class="card-title">UNIKOM Dago</span>
+														<img class="logo"
+															 src="<?= base_url() ?>assets/images/360.png"/>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--                Button Left Right-->
+				<!--                <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev"> <span-->
+				<!--                        class="carousel-control-prev-icon" aria-hidden="true"></span> <span-->
+				<!--                        class="sr-only">Previous</span> </a>-->
+				<!--                <a-->
+				<!--                        class="carousel-control-next" href="#myCarousel" role="button" data-slide="next"> <span-->
+				<!--                        class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span>-->
+				<!--                </a>-->
+			</div>
+		</div>
+	</div>
+</div>
+
